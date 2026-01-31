91cb7a34ad130cd7b35286d8caad41c645e445a2
Skullkid <momiamfree@gmail.com>
2026-01-31 11:12:58 +0100

    Init React frontend with Vite

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1 className="text-4xl font-bold text-center text-blue-600">
+        Tailwind OK 🚀
+      </h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
